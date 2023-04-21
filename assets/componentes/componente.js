9dbc1ce3570b6578f04e6cf5b95a7fe30750640a
Juan Avila <123758906+JuanDavidAvilaRaveloCampus@users.noreply.github.com>
2023-04-21 03:31:59 +0000

    feat(new funtion): :sparkles: api
    add the funtion at the api

--- assets/componentes/componente.js
+++ assets/componentes/componente.js
@@ -1,39 +1,64 @@
 import config from "./config.js";
-let storage = JSON.parse(localStorage.getItem('data'))
+let storage = JSON.parse(localStorage.getItem('data'));
+
 
 let contenedor_pokemones = document.querySelector('section');
-
-
 
 export default {
     show_all(){
         config.storage()
-        function fetch_Pokemon(id) {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-            .then(res => res.json())
-            .then(data => console.log(data))
-        }
-        //como respuesta debe almacenar una data específica
-        fetch_Pokemon(1)
 
-        function number_pokemons(number){
-            for (let i = 0; i < number; i++) {
-                fetch_Pokemon(i)
+        document.querySelector('#formulario').addEventListener('submit', (e)=>{
+            e.preventDefault();
+
+            let formulario = Object.fromEntries(new FormData(e.target));
+
+            console.log(formulario);
+            
+            fetch_Pokemon(`${formulario.buscador}`);
+
+            function fetch_Pokemon(id) {
+                fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+                .then(res => res.json())
+                .then(data => console.log(data))
             }
-        }
 
-        function create_pokemon(pokemon){
-            let card = document.createElementNS('div')
-            card.classList.add('pokemon_block')
+            function number_pokemons(number){
+                for (let i = 0; i < number; i++) {
+                    fetch_Pokemon(i)
+                }
+            }
+    
+            function create_pokemon(pokemon){
+                let card = document.createElementNS('div')
+                card.classList.add('pokemon_block')
+    
+                let sprite_container = document.createElement('div');
+                sprite_container.classList.add('img_container')
+    
+                let sprite = document.createElement('img');
+                sprite.src = pokemon.sprites.front_default;
+    
+                sprite_container.appendChild(sprite);
+    
+                let number = document.querySelector('span')
+                number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
+    
+                let name = document.querySelector('h2')
+                // name.classList.add('name');
+                name.textContent = pokemon.name
+    
+                card.appendChild(sprite_container);
+                card.appendChild(number),
+                card.appendChild(name);
+                pokemon_container(card)
+            }
 
-            let srite_container = document.createElement('div');
-            srite_container.classList.add('img_container')
+            // number_pokemons('pikachu')
 
-            let sprite = document.createElement('img');
-            sprite.src = pokemon.sprites.front_default;
-        }
 
-        number_pokemons(9)
+        })
+        
     }
 }
 
