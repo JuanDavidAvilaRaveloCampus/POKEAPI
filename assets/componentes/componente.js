962a656cd99d847384731cc3e13ae810dd747e57
Juan Avila <123758906+JuanDavidAvilaRaveloCampus@users.noreply.github.com>
2023-05-02 09:51:55 +0000

    feat: :bug: no recuerdo
    tengo cule sueño

--- assets/componentes/componente.js
+++ assets/componentes/componente.js
@@ -1,6 +1,24 @@
 import config from "./config.js";
 // import 'animate.css';
-let storage = JSON.parse(localStorage.getItem('data'));
+// let storage = JSON.parse(localStorage.getItem('data'));
+
+
+export default {
+    show_all() {
+        // config.storage()
+
+        let formulario = document.querySelector('#formulario').addEventListener('submit', (e) => {
+            e.preventDefault();
+            let formulario = Object.fromEntries(new FormData(e.target));
+            fetch_Pokemon(`${formulario.buscador.toLowerCase()}`);
+        })
+
+        if (formulario == ''){
+            table_pokemons();
+        }
+    }
+}
+
 
 const fetch_Pokemon = async (name) => {
     try {
@@ -9,7 +27,8 @@ const fetch_Pokemon = async (name) => {
         // let nameentificador = await data.name;
 
         let data = await res.json();
-        let nombre = name;
+        let id = data.id
+        let nombre = data.name;
         let vida = data.stats[0].base_stat;
         let ataque = data.stats[1].base_stat;
         let velocidad = data.stats[5].base_stat;
@@ -17,51 +36,60 @@ const fetch_Pokemon = async (name) => {
         let ataque_especial = data.stats[3].base_stat;
         let defensa_especial = data.stats[4].base_stat;
 
+        pintarCard(
+            id, 
+            nombre, 
+            vida, 
+            ataque, 
+            velocidad, 
+            defensa, 
+            ataque_especial, 
+            defensa_especial
+        );
 
-        // let estadisticas = await data.stats
-        // let hp = await data.stats[0].base_stat;
-
-        // console.log(data);
-
-        // console.log({
-        //     nombre : name,
-        //     vida : data.stats[0].base_stat,
-        //     ataque : data.stats[1].base_stat,
-        //     velocidad : data.stats[5].base_stat,
-        //     defensa : data.stats[2].base_stat,
-        //     ataque_especial : data.stats[3].base_stat,
-        //     defensa_especial : data.stats[4].base_stat
-        // });
-        pintarCard(data.id, nombre, vida, ataque, velocidad, defensa, ataque_especial, defensa_especial);
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
 };
 
-function pintarCard(
-    id,
-    nombre,
-    vida,
-    ataque,
-    velocidad,
-    defensa,
-    ataque_especial,
-    defensa_especial
-) {
-    console.log(id);
-    //INSETA EL VALOR DE LA VIDA EN EL CSS
-    // let barra_vida = document.querySelector('#porcentaje_vida');
-    // porcentaje_vida = Number(vida);
-    // barra_vida.style.width = porcentaje_vida + '%'
-    // for (let i = 0; i < 20; i++) {
-        // const element = array[i];
+let contador = 0;
 
+// function table_pokemons() {
+    
+//     for (let i = 0; i < 20; i++) {
+//         contador = i;
+
+//         if (contador != 0 && contador == 20) {
+            
+//         }
+//     }
+
+// }
+
+function pintarCard(
+    id, 
+    nombre, 
+    vida, 
+    ataque, 
+    velocidad, 
+    defensa, 
+    ataque_especial, 
+    defensa_especial
+){
+    console.log(id);
         document.querySelector('#cards').insertAdjacentHTML('beforeend', `   
             <div class="card">
                 <img src="./assets/img/img_banner_pokemon_logo/248737.jpg" alt="" class="img_sobre">
                 
                 <div class="info_card">
-                    <h2>${nombre.toUpperCase()}</h2>
+                    
+                    <div class="name_id">
+                        <h2>${nombre}</h2>
+                        <span>#${id}</span>
+
+                    </div>
+
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="" class="img_card">
                     <div class="stadistics">
                         <h3>HP</h3>
@@ -112,23 +140,27 @@ function pintarCard(
             </div>
         `)
 
-
         document.querySelector('.card').addEventListener('mouseover',()=>{
             let porcentaje_vida = document.querySelector('#porcentaje_vida');
             let porcentaje_ataque = document.querySelector('#porcentaje_ataque');
             let porcentaje_defensa = document.querySelector('#porcentaje_defensa');
             let porcentaje_velocidad = document.querySelector('#porcentaje_velocidad');
-            let 
-            
+            let porcentaje_ataque_especial = document.querySelector('#porcentaje_ataque_especial');
+            let porcentaje_defensa_especial = document.querySelector('#porcentaje_defensa_especial');
+
             porcentajes_animacion(porcentaje_vida,vida);
             porcentajes_animacion(porcentaje_ataque,ataque);
-
+            porcentajes_animacion(porcentaje_defensa,defensa);
+            porcentajes_animacion(porcentaje_velocidad,velocidad);
+            porcentajes_animacion(porcentaje_ataque_especial,ataque_especial);
+            porcentajes_animacion(porcentaje_defensa_especial,defensa_especial);
             
         });
 
 
 
 }
+
 //EL OBJETIVO ES HACER QUE LAS BARRAS TENGAN UNA ANIMACIÓN DINÁMICA
 function porcentajes_animacion(elemento,ancho){
     elemento.style.transition = `width 1s ease`;
@@ -206,21 +238,5 @@ function create_pokemon(pokemon) {
 //         `)
 // }
 
-export default {
-    show_all() {
-        config.storage()
-
-        document.querySelector('#formulario').addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            let formulario = Object.fromEntries(new FormData(e.target));
-
-
-
-            fetch_Pokemon(`${formulario.buscador.toLowerCase()}`);
-        },
-        )
-    }
-}
 
 
